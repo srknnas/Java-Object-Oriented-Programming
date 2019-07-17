43efaf5961c07f294e0b1f7880659bedd03363d3
srknnas <48711124+srknnas@users.noreply.github.com>
2019-07-17 17:16:42 +0300

    Add files via upload

---
+++ JavaExample_UML/src/Insan.java
@@ -0,0 +1,15 @@
+public class Insan {
+
+    private int yas;
+
+    private int boy;
+
+    private int kilo;
+
+    public int yemek() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void calismak() {
+    }
+}
---
+++ JavaExample_UML/src/Ogrenci.java
@@ -0,0 +1,9 @@
+public class Ogrenci extends Insan {
+
+    private int ortalama;
+
+    private String bolum;
+
+    public void dersCalismak() {
+    }
+}
