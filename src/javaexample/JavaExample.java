1e86f005c0b299a04485cabad373475cd97d18e2
srknnas <48711124+srknnas@users.noreply.github.com>
2019-07-17 16:41:45 +0300

    Add files via upload

---
+++ src/javaexample/JavaExample.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample;
+
+/**
+ *
+ * @author SERKANNAS
+ */
+public class JavaExample {
+    
+    // Create an array
+    private final static int SIZE = 15;
+    private int[] arrayOfInts = new int[SIZE];
+    
+    //Constructor
+    public JavaExample() {
+        // fill the array with ascending integer values
+        for (int i = 0; i < SIZE; i++) {
+            arrayOfInts[i] = i;
+        }
+    }
+    
+    public void printEven() {
+        
+        // Print out values of even indices of the array
+        DataStructureIterator iterator = this.new EvenIterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+    
+    interface DataStructureIterator extends java.util.Iterator<Integer> { } 
+
+    // Inner class implements the DataStructureIterator interface,
+    // which extends the Iterator<Integer> interface
+    
+    private class EvenIterator implements DataStructureIterator {
+        
+        // Start stepping through the array from the beginning
+        private int nextIndex = 0;
+        
+        @Override
+        public boolean hasNext() {
+            
+            // Check if the current element is the last in the array
+            return (nextIndex <= SIZE - 1);
+        }        
+        
+        public Integer next() {
+            
+            // Record a value of an even index of the array
+            Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
+            
+            // Get the next even element
+            nextIndex += 2;
+            return retValue;
+        }
+    }
+    
+    public static void main(String s[]) {
+        
+        // Fill the array with integer values and print out only
+        // values of even indices
+        JavaExample ds = new JavaExample();
+        ds.printEven();        
+    }
+}
